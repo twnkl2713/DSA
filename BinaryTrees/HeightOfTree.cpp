83faa7bdd034f60f4e140cad9df506a7a4455bc9
twinkle485 <twinklepalbrb@yahoo.com>
2025-06-06 09:11:14 +0530

    green

---
+++ BinaryTrees/HeightOfTree.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+int height(Node* root) {
+    if(root == NULL) {
+        return -1;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return max(leftHeight, rightHeight) + 1;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    cout << "Height of the tree: " << height(root) << endl;
+
+    return 0;
+}
