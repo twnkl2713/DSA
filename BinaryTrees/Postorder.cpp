4f17289883e5c26e2a5c8edcc0a54b6755eaa07b
twinkle485 <twinklepalbrb@yahoo.com>
2025-06-06 08:14:59 +0530

    save trees

---
+++ BinaryTrees/Postorder.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void preorder(Node* root) {
+    if (root == nullptr)
+        return;
+ 
+    preorder(root->left);  
+    preorder(root->right);
+    cout << root->data << " ";   
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+
+    cout << "Preorder Traversal: ";
+    preorder(root);
+    cout << endl;
+
+    return 0;
+}
