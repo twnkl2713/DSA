d60b7e3d8c3dfe393ffecb2d7c7a4cb3a8a4c75d
twinkle485 <twinklepalbrb@yahoo.com>
2025-06-09 10:48:02 +0530

    revision needed

--- BinarySearchTrees/BST_from_Preorder.cpp
+++ BinarySearchTrees/BST_from_Preorder.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 
 struct Node {
@@ -13,6 +14,49 @@ struct Node {
     }
 };
 
+Node* buildBST(vector<int> &preorder, int &index, int min, int max) {
+    if(index >= preorder.size()) {
+        return NULL;
+    }
+    
+    int currNode = preorder[index];
+    if(currNode < min || currNode > max) {
+        return NULL;
+    }
+
+    Node* root = new Node(currNode);
+    index++;
+
+    // all nodes in left st must be < root->data
+    root->left = buildBST(preorder, index, min, currNode-1);
+    // all nodes in right st must be > root->data
+    root->right = buildBST(preorder, index, currNode+1, max);
+
+    return root;
+}
+
+Node* bstFromPreorder(vector<int>& preorder) {
+    int index = 0;
+    return buildBST(preorder, index, INT_MIN, INT_MAX);
+}
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    vector<int> preorder = {8, 5, 1, 7, 10, 12};
+    Node* root = bstFromPreorder(preorder);
+
+    cout << "Inorder traversal of constructed BST: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
 
 
 
