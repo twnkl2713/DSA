99500e07a7d6633407696048730acfbb166d6814
twinkle485 <twinklepalbrb@yahoo.com>
2025-06-25 13:25:07 +0530

    revise rotten oranges

--- Graphs/BFS_DFS/RottenOranges.cpp
+++ Graphs/BFS_DFS/RottenOranges.cpp
@@ -7,25 +7,26 @@ int orangesRotting(vector<vector<int>>& grid) {
 
     queue<pair<pair<int, int>, int>> q;
     vector<vector<int>> vis(n, vector<int>(m, 0));
-    int cntfresh = 0;
+    int cntfresh = 0; // for fresh oranges
 
     // Initialize queue and count fresh oranges
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             if(grid[i][j] == 2) {
-                q.push({{i, j}, 0});
-                vis[i][j] = 2;
+                q.push({{i, j}, 0}); // add all initially rotten oranges to the queue
+                vis[i][j] = 2; // mark their position as visited
             } else {
                 vis[i][j] = 0;
             }
             if(grid[i][j] == 1) {
-                cntfresh++;
+                cntfresh++; // count the number of fresh oranges
             }
         }
     }
 
     int time = 0;
     int cnt = 0;
+    // the 4 directions: up, right, down, left
     int drow[] = {-1, 0, 1, 0};
     int dcol[] = {0, 1, 0, -1};
 
@@ -37,20 +38,21 @@ int orangesRotting(vector<vector<int>>& grid) {
         q.pop();
         time = max(time, t);
 
+        // for each rotten orange in the queue: extract its position and time; update the total time(max across all BFS levels)
         for(int i = 0; i < 4; i++) {
             int nrow = r + drow[i];
             int ncol = c + dcol[i];
 
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-                && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-
+            // loop through the 4 directions to check neighbours
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
+                // make the fresh orange rotten and push it into the queue with time t+1
                 q.push({{nrow, ncol}, t + 1});
-                vis[nrow][ncol] = 2;
-                cnt++;
+                vis[nrow][ncol] = 2; // mark as visited
+                cnt++; // count of rotted fresh oranges
             }
         }
     }
-    if(cnt != cntfresh) return -1;
+    if(cnt != cntfresh) return -1; //it means some oranges couldn't be rotted
     return time;
 }
 
